2f180f34382949f62dd3cc33d189a29ff8da563d
alu0100505092 <alu0100505092@ull.edu.es>
2015-12-08 14:32:42 +0000

    Modificado bibliografia.rb

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -1,4 +1,10 @@
 require "bibliografia/version"
+require "bibliografia/lista"
+require "bibliografia/publicaciones_periodicas"
+require "bibliografia/libro"
+require "bibliografia/documento_electronico"
+require "bibliografia/articulo_periodico"
+require "bibliografia/articulo_revista"
 
 module Bibliografia
   # Your code goes here...
